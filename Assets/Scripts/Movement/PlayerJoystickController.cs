18eba4969fdaecf2b56d8f3e5190842ba5ecf247
Muhammad Rauf <pahry182@gmail.com>
2022-10-23 21:40:24 +0700

    udapete

--- Assets/Scripts/Movement/PlayerJoystickController.cs
+++ Assets/Scripts/Movement/PlayerJoystickController.cs
@@ -8,17 +8,31 @@ public class PlayerJoystickController : MonoBehaviour
     public float _moveSpeed;
 
     [SerializeField] private FixedJoystick _joystick;
-    private Rigidbody2D _rigidbody;    
+    private Rigidbody2D _rigidbody;
+    public Animator anim;
 
-    
+
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+
     private void FixedUpdate()
     {
         _rigidbody.velocity = new Vector2(_joystick.Horizontal * _moveSpeed, _joystick.Vertical * _moveSpeed);
     }
+
 }
